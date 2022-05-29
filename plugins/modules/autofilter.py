563b4b41b273dce281faf66531d014040964c4b1
king legend <49789123+legendoftelegram@users.noreply.github.com>
2022-05-29 16:29:06 +0530

    Update autofilter.py

--- plugins/modules/autofilter.py
+++ plugins/modules/autofilter.py
@@ -51,15 +51,15 @@ async def group_filters(client, message):
 
             imdb=await get_poster(search)
             if imdb and imdb.get('poster'):
-                dell=await message.reply_photo(photo=random.choice(BOT_PICS)), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+                dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(600)
                 await dell.delete()
             elif imdb:
-                dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+                dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(600)
                 await dell.delete()
             else:
-                dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_2.format(query=search, mention=message.from_user.mention, chat=message.chat.title), reply_markup=InlineKeyboardMarkup(buttons))
+                dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(600)
                 await dell.delete()
             return
@@ -76,15 +76,15 @@ async def group_filters(client, message):
 
         imdb=await get_poster(search)
         if imdb and imdb.get('poster'):
-            dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+            dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(300)
             await dell.delete()        
         elif imdb:
-            dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+            dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(300)
             await dell.delete()
         else:
-            dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_2.format(query=search, mention=message.from_user.mention, chat=message.chat.title), reply_markup=InlineKeyboardMarkup(buttons))
+            dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(300)
             await dell.delete()
 
@@ -135,15 +135,15 @@ async def pm_autofilter(client, message):
 
             imdb=await get_poster(search)
             if imdb and imdb.get('poster'):
-                dell=await message.reply_photo(photo=imdb.get('poster'), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+                dell=await message.reply_photo(photo=imdb.get('poster'), reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(300)
                 await dell.delete()
             elif imdb:
-                dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+                dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(300)
                 await dell.delete()
             else:
-                dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_2.format(query=search, mention=message.from_user.mention, chat=bot_info.BOT_NAME), reply_markup=InlineKeyboardMarkup(buttons))
+                dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(300)
                 await dell.delete()
 
@@ -161,14 +161,14 @@ async def pm_autofilter(client, message):
 
         imdb=await get_poster(search)
         if imdb and imdb.get('poster'):
-            dell=await message.reply_photo(photo=imdb.get('poster'), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+            dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(300)
             await dell.delete()       
         elif imdb:
-            dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_1.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url']), reply_markup=InlineKeyboardMarkup(buttons))
+            dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(300)
             await dell.delete()
         else:
-            dell=await message.reply_photo(photo=random.choice(BOT_PICS), caption=AtwFilt.GET_MOVIE_2.format(query=search, mention=message.from_user.mention, chat=bot_info.BOT_NAME), reply_markup=InlineKeyboardMarkup(buttons))
+            dell=await message.reply_photo(photo=random.choice(BOT_PICS), reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(300)
             await dell.delete()
